4e1a44592214f3643b3bffd6ffc85a4b5b1aa4ba
scasouls <sanhun430@outlook.com>
2025-05-27 16:26:45 +0800

    proj1的简单完成

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -1,4 +1,133 @@
 package deque;
 
-public class ArrayDeque {
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class ArrayDeque<T> implements  Deque<T> ,Iterable<T> {
+
+    private static final int DEFAULT_CAPACITY = 8;
+
+    private T[] array;
+    private int front;
+    private int rear;
+    private int size;
+    private int capacity;
+
+    @SuppressWarnings("unchecked")
+    public ArrayDeque() {
+        capacity = DEFAULT_CAPACITY;
+        array    = (T[]) new Object[capacity];
+        front = rear = size = 0;
+    }
+
+    public ArrayDeque(int cap){
+        capacity = cap;
+        array = (T[]) new Object[capacity];
+        front = rear = size = 0;
+    }
+
+    @Override
+    public int     size()        { return size;     }
+
+    @SuppressWarnings("unchecked")
+    private void resize(int newCap) {
+        T[] newArr = (T[]) new Object[newCap];
+        for (int i = 0; i < size; i++) {
+            newArr[i] = array[(front + i) % capacity];
+        }
+        array    = newArr;
+        capacity = newCap;
+        front    = 0;
+        rear     = size;
+    }
+
+    private void maybeShrink() {
+        if (capacity > DEFAULT_CAPACITY && size <= capacity / 4) {
+            resize(capacity / 2);
+        }
+    }
+
+    @Override
+    public void addFirst(T item) {
+        if (size == capacity) resize(capacity * 2);
+        front = (front - 1 + capacity) % capacity; // 先递减索引再写入
+        array[front] = item;
+        size++;
+    }
+
+    @Override
+    public void addLast(T item) {
+        if (size == capacity) resize(capacity * 2);
+        array[rear] = item;
+        rear = (rear + 1) % capacity;
+        size++;
+    }
+
+    @Override
+    public T removeFirst() {
+        if (isEmpty()) return null;
+        T val = array[front];
+        array[front] = null;
+        front = (front + 1) % capacity;
+        size--;
+        maybeShrink();
+        return val;
+    }
+
+    @Override
+    public T removeLast() {
+        if (isEmpty()) return null;
+        rear = (rear - 1 + capacity) % capacity;
+        T val = array[rear];
+        array[rear] = null;
+        size--;
+        maybeShrink();
+        return val;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size) return null;
+        return array[(front + index) % capacity];
+    }
+
+    @Override
+    public void printDeque() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(array[(front + i) % capacity] + " ");
+        }
+        System.out.println();
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private int idx = 0;
+            @Override public boolean hasNext()             { return idx < size; }
+            @Override public T next() {
+                if (!hasNext()) throw new NoSuchElementException();
+                return array[(front + idx++) % capacity];
+            }
+        };
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;                      // 同一个对象
+        if (!(o instanceof ArrayDeque<?> other)) return false; // 类型检查
+
+        if (this.size != other.size) return false;       // 长度不同
+
+        for (int i = 0; i < size; i++) {
+            T thisItem = this.get(i);
+            Object otherItem = other.get(i);
+            if (thisItem == null) {
+                if (otherItem != null) return false;
+            } else {
+                if (!thisItem.equals(otherItem)) return false;
+            }
+        }
+
+        return true;
+    }
 }
--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -1,4 +1,148 @@
 package deque;
 
-public class LinkedListDeque {
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class LinkedListDeque<T> implements Deque<T> ,Iterable<T> {
+
+    private static class Node<T> {
+        T value;
+        Node<T> prev;
+        Node<T> next;
+
+        Node(T v) { value = v; }
+    }
+
+    private final Node<T> sentinel;   // 只保存头尾引用
+    private int size;
+
+    public LinkedListDeque() {
+        sentinel = new Node<>(null);
+        sentinel.prev = null;   // head
+        sentinel.next = null;   // tail
+        size = 0;
+    }
+
+    @Override
+    public void addFirst(T item) {
+        Node<T> newNode = new Node<>(item);
+        newNode.next = sentinel.prev;   // 原 head
+        newNode.prev = null;
+
+        if (sentinel.prev != null) {          // 非空
+            sentinel.prev.prev = newNode;
+        } else {                              // 空表，头=尾
+            sentinel.next = newNode;
+        }
+        sentinel.prev = newNode;              // 更新 head
+        size++;
+    }
+
+    @Override
+    public void addLast(T item) {
+        Node<T> newNode = new Node<>(item);
+        newNode.prev = sentinel.next;   // 原 tail
+        newNode.next = null;
+
+        if (sentinel.next != null) {          // 非空
+            sentinel.next.next = newNode;
+        } else {                              // 空表，头=尾
+            sentinel.prev = newNode;
+        }
+        sentinel.next = newNode;              // 更新 tail
+        size++;
+    }
+
+    @Override
+    public int     size()     { return size; }
+
+    @Override
+    public void printDeque() {
+        for (Node<T> p = sentinel.prev; p != null; p = p.next) {
+            System.out.print(p.value + " ");
+        }
+        System.out.println();
+    }
+
+    @Override
+    public T removeFirst() {
+        if (isEmpty()) return null;
+        Node<T> head = sentinel.prev;
+        T val = head.value;
+
+        Node<T> newHead = head.next;
+        if (newHead != null) {
+            newHead.prev = null;
+        } else {                     // 删除后变空
+            sentinel.next = null;
+        }
+        sentinel.prev = newHead;
+        size--;
+        return val;
+    }
+
+    @Override
+    public T removeLast() {
+        if (isEmpty()) return null;
+        Node<T> tail = sentinel.next;
+        T val = tail.value;
+
+        Node<T> newTail = tail.prev;
+        if (newTail != null) {
+            newTail.next = null;
+        } else {                     // 删除后变空
+            sentinel.prev = null;
+        }
+        sentinel.next = newTail;
+        size--;
+        return val;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size) return null;
+        Node<T> p = sentinel.prev;           // head
+        for (int i = 0; i < index; i++) {
+            p = p.next;
+        }
+        return p.value;
+    }
+
+    public T getRecursive(int index) {
+        if (index < 0 || index >= size) return null;
+        return getRecHelper(sentinel.prev, index);
+    }
+
+    private T getRecHelper(Node<T> node, int idx) {
+        return (idx == 0) ? node.value : getRecHelper(node.next, idx - 1);
+    }
+
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<>() {
+            private Node<T> cur = sentinel.prev;      // 从头开始
+            @Override public boolean hasNext() { return cur != null; }
+            @Override public T next() {
+                if (!hasNext()) throw new NoSuchElementException();
+                T val = cur.value;
+                cur = cur.next;
+                return val;
+            }
+        };
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof LinkedListDeque<?> other)) return false;
+        if (this.size != other.size) return false;
+
+        Iterator<T> it1 = this.iterator();
+        Iterator<?> it2 = other.iterator();
+        while (it1.hasNext() && it2.hasNext()) {
+            if (!it1.next().equals(it2.next())) return false;
+        }
+        return true;
+    }
 }
